a6791ba2c861953923e6dc86db50ebcea7e5e560
Ryosuke Mizuno <ryo.mizuno502@gmail.com>
2021-03-02 16:43:12 +0900

    [追加]メニューバー

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NextPage } from 'next'
 import Head from 'next/head'
 import styles from 'src/styles/Home.module.scss'
 
 const Home: NextPage = () => {
+  const [isopen, setIsOpen] = useState(false)
+
+  const openMenu = (): void => {
+    setIsOpen(!isopen)
+    console.log(isopen)
+  }
+
   const array: number[] = []
   for (let i = 1; i < 101; i++) {
     array.push(i)
@@ -16,7 +23,14 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.main_left}></div>
+        <div className={styles.main_left}>
+          <div
+            className={styles.menubar}
+            style={{ left: isopen ? '-50px' : '-150px' }}
+          >
+            <button className={styles.js_menu} onClick={openMenu}></button>
+          </div>
+        </div>
         <div className={styles.main_right}>
           {array.map((num, index) => (
             <p key={index} className={styles.txt}>
